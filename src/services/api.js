5b8f31def057c6874e4b5683cb102e49f9f1cbbc
lequangsonNodejss <lequangson27092003@gmail.com>
2025-08-13 13:38:46 +0700

    Use full API URL for mockfast

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 export const fetchApiData = () => {
-  return fetch("/backend/apitemplate/get/ZCKZ8H8MTA")
+  return fetch("https://mockfast.io/backend/apitemplate/get/ZCKZ8H8MTA")
     .then((res) => {
       if (!res.ok) throw new Error("HTTP error " + res.status);
       return res.json();
