03d39e7f2872daaf376acff576309c18a7e85be6
lequangsonNodejss <lequangson27092003@gmail.com>
2025-08-13 13:55:05 +0700

    Add serverless API route for Mockfast

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 export const fetchApiData = () => {
-  return fetch("https://mockfast.io/backend/apitemplate/get/ZCKZ8H8MTA")
+  return ffetch("/api/mockfast")
+
     .then((res) => {
       if (!res.ok) throw new Error("HTTP error " + res.status);
       return res.json();
